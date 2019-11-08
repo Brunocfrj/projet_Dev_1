2d753a9632d3e8e180d6ec4f30a387204141fa82
bruno <bruno@DESKTOP-03B2SNB>
2019-11-07 23:04:54 -0200

    Aqui é meu primeiro comit

---
+++ MeusTest_01/src/meustest_01/aplher.java
@@ -0,0 +1,2 @@
+public class Morador...{....public morador(String no, String ap,String te, int an){ nomeCompleto = no;  apartamento  = ap;  telefone     = te;  anoChegada   = an; }public int  permanencia()   { return (1997 - anoChegada); } }
+
